c355ff6248d777cbf8fd7e9c16b4cba961bbb6db
Nitesh Kumar Gupta <146430887+niteshg0@users.noreply.github.com>
2025-02-24 14:12:24 +0530

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "https://alumni-backend-8eqk.onrender.com"
-export const BASE_URL = ""
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL
 export const USERS_URL = "/api/users"
 export const ACADMIC_URL = "/api/academics"
 export const JOBDETAIL_URL = "/api/jobDetail"
